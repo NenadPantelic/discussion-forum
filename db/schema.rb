f61582cc60dfa8d7aa971c44c2bc987a3e65b5ea
NenadPantelic <nenadpantelickg@gmail.com>
2020-10-29 14:13:32 +0100

    Add discussions scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_125412) do
+ActiveRecord::Schema.define(version: 2020_10_29_130130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
