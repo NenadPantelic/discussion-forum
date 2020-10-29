6cc2e5ba814139084cb92fb683f55259d14c0a34
NenadPantelic <nenadpantelickg@gmail.com>
2020-10-29 15:14:22 +0100

    Add other models and make relations between them

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,5 +8,6 @@ class RegistrationsController < Devise::RegistrationsController
     def account_update_params
         params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
     end
+    
 
 end
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,2 +1,9 @@
 class Discussion < ApplicationRecord
+    # add unique constraint to discussion title and channel name
+    belongs_to :channel
+    belongs_to :user
+    has_many :replies, dependent: :destroy
+    valdates :title, :content, presence: true
+
+    
 end
